b745a77202404520bcd4552c360e5153235adb37
RAVI SHARMA <ravics09@RAVIs-MacBook-Air.local>
2021-09-22 17:26:21 +0530

    Corrected navbar with react-bootstrap and with class state

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,80 +2,59 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: true,
+      userName:"Ravi"
+    };
+  }
+
+  logout = () => {
+    this.setState({currentUser: false});
+  }
+
   render() {
+    const { currentUser, userName } = this.state;
     return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <Link class="navbar-brand" to="/">
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
             JavaScript Centric
           </Link>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarScroll"
-            aria-controls="navbarScroll"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
-              <li class="nav-item">
-                <Link class="nav-link" to="/signin">
-                  SignIn
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/signup">
-                  SignUp
-                </Link>
-              </li>
-            </ul>
+          <div className="navbar-nav mr-auto">
+            {currentUser ? (
+              <div className="navbar-nav ml-auto d-flex">
+                <li className="nav-item">
+                  <Link to={"/profile"} className="nav-link">
+                    {userName}
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/signin" className="nav-link" onClick={this.logout}>
+                    Logout
+                  </Link>
+                </li>
+              </div>
+            ) : (
+              <div className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to={"/signin"} className="nav-link">
+                    Sign In
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to={"/signup"} className="nav-link">
+                    Sign Up
+                  </Link>
+                </li>
+              </div>
+            )}
           </div>
         </div>
       </nav>
     );
   }
 }
-
-// class NavBar extends Component {
-//   render() {
-//     return (
-//       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-//         <div class="container-fluid">
-//           <Link class="navbar-brand" to="/">
-//             JavaScript Centric
-//           </Link>
-//           <button
-//             class="navbar-toggler"
-//             type="button"
-//             data-bs-toggle="collapse"
-//             data-bs-target="#navbarScroll"
-//             aria-controls="navbarScroll"
-//             aria-expanded="false"
-//             aria-label="Toggle navigation"
-//           >
-//             <span class="navbar-toggler-icon"></span>
-//           </button>
-//           <div class="collapse navbar-collapse" id="navbarScroll">
-//             <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
-//               <li class="nav-item">
-//                 <Link class="nav-link" to="/signin">
-//                   SignIn
-//                 </Link>
-//               </li>
-//               <li class="nav-item">
-//                 <Link class="nav-link" to="/signup">
-//                   SignUp
-//                 </Link>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </nav>
-//     );
-//   }
-// }
 export default NavBar;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import SignIn from "./../pages/SignIn/signIn";
 import SignUp from "./../pages/SignUp/signUp";
 import Home from "./../pages/Home/home";
+import Profile from "./../pages/Profile/profile";
 
 const Routes = () => {
   return (
@@ -12,6 +13,7 @@ const Routes = () => {
       <Route exact component={Home} path="/home" />
       <Route exact component={SignIn} path="/signin" />
       <Route exact component={SignUp} path="/signup" />
+      <Route exact component={Profile} path="/profile" />
     </Switch>
   );
 };
